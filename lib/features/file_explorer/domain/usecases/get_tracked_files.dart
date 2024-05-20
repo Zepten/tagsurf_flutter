d2d7ce2d58bfb52ca58d89b936bec13e5d61be76
Zepten <Zepten@yandex.ru>
2024-05-20 21:45:28 +0300

    rename GetTrackedFilesUseCase

--- lib/features/file_explorer/domain/usecases/get_tracked_files.dart
+++ lib/features/file_explorer/domain/usecases/get_tracked_files.dart
@@ -2,10 +2,10 @@ import 'package:tagsurf_flutter/features/file_explorer/core/usecase/usecase.dart
 import 'package:tagsurf_flutter/features/file_explorer/domain/entities/file_entity.dart';
 import 'package:tagsurf_flutter/features/file_explorer/domain/repository/file_repository.dart';
 
-class GetTrackedFileUseCase implements UseCase<List<FileEntity>, void> {
+class GetTrackedFilesUseCase implements UseCase<List<FileEntity>, void> {
   final FileRepository _fileRepository;
 
-  GetTrackedFileUseCase(this._fileRepository);
+  GetTrackedFilesUseCase(this._fileRepository);
 
   @override
   Future<List<FileEntity>> call({void params}) {
