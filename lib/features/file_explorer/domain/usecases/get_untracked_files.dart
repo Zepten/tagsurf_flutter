e1b337395484d9911217655ac9df04340f7d5450
Zepten <Zepten@yandex.ru>
2024-05-21 00:28:28 +0300

    add use case for untracked files

---
+++ lib/features/file_explorer/domain/usecases/get_untracked_files.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+import 'package:tagsurf_flutter/features/file_explorer/core/usecase/usecase.dart';
+import 'package:tagsurf_flutter/features/file_explorer/domain/entities/file_entity.dart';
+import 'package:tagsurf_flutter/features/file_explorer/domain/repository/file_repository.dart';
+
+class GetUnrackedFilesUseCase implements UseCase<List<FileEntity>, void> {
+  final FileRepository _fileRepository;
+
+  GetUnrackedFilesUseCase(this._fileRepository);
+
+  @override
+  Future<List<File>> call({void params}) { // TODO: FIX THIS
+    return _fileRepository.getFiles(params!);
+  }
+}
