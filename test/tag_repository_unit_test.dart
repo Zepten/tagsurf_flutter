d3d0403d4d38f17d3dd1130b74d4e554dd697a6e
Zepten <Zepten@yandex.ru>
2024-05-24 21:38:19 +0300

    add unit tests for tag repository

---
+++ test/tag_repository_unit_test.dart
@@ -0,0 +1,122 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tagsurf_flutter/features/file_explorer/data/data_sources/database/app_database.dart';
+import 'package:tagsurf_flutter/features/file_explorer/data/repository/tag_repository_impl.dart';
+import 'package:tagsurf_flutter/features/file_explorer/domain/entities/tag_entity.dart';
+
+Future<void> main() async {
+  group('Test tag repository', () {
+    late AppDatabase database;
+    late TagRepositoryImpl tagRepository;
+
+    setUp(() async {
+      // Database and tag repository initialization
+      database = await $FloorAppDatabase.inMemoryDatabaseBuilder().build();
+      // database = await $FloorAppDatabase.databaseBuilder('app_database.db').build();
+      tagRepository = TagRepositoryImpl(database);
+    });
+
+    tearDown(() async {
+      database.close();
+    });
+
+    test('Create tag', () async {
+      const tag =
+          TagEntity(name: 'test tag', parentTagName: null, colorCode: '#ffffff');
+
+      // Create tag
+      await tagRepository.createTag(tag);
+
+      // Check if tag is created
+      final actual = await tagRepository.getTagByName(tag.name!);
+      expect(actual, equals(tag));
+    });
+
+    test('Update tag', () async {
+      const tag =
+          TagEntity(name: 'test tag', parentTagName: null, colorCode: '#ffffff');
+
+      // Create tag
+      await tagRepository.createTag(tag);
+      final actualCreated = await tagRepository.getTagByName(tag.name!);
+      expect(actualCreated, equals(tag));
+
+      // Update tag
+      const tagUpdated =
+          TagEntity(name: 'test tag', parentTagName: null, colorCode: '#000000');
+      await tagRepository.updateTag(tagUpdated);
+
+      // Check if tag is updated
+      final actualUpdated = await tagRepository.getTagByName(tagUpdated.name!);
+      expect(actualUpdated, equals(tagUpdated));
+    });
+
+    test('Delete tag', () async {
+      const tag =
+          TagEntity(name: 'test tag', parentTagName: null, colorCode: '#ffffff');
+
+      // Create tag
+      await tagRepository.createTag(tag);
+      final actualCreated = await tagRepository.getTagByName(tag.name!);
+      expect(actualCreated, equals(tag));
+
+      // Delete tag
+      await tagRepository.deleteTag(tag);
+
+      // Check if tag is deleted
+      final actualDeleted = await tagRepository.getTagByName(tag.name!);
+      expect(actualDeleted, isNull);
+    });
+
+    test('Get all tags', () async {
+      const tags = [
+        TagEntity(name: 'test tag 1', parentTagName: null, colorCode: '#ff0000'),
+        TagEntity(name: 'test tag 2', parentTagName: null, colorCode: '#00ff00'),
+        TagEntity(name: 'test tag 3', parentTagName: null, colorCode: '#0000ff'),
+      ];
+
+      // Create tags
+      for (final tag in tags) {
+        await tagRepository.createTag(tag);
+      }
+
+      // Get all tags
+      final actual = await tagRepository.getAllTags();
+
+      // Check if returned tags are the same as test tags
+      expect(actual, equals(tags));
+    });
+
+    test('Get parent tag', () async {
+      const parentTag = TagEntity(
+          name: 'test parent tag', parentTagName: null, colorCode: '#ffffff');
+      const childTag = TagEntity(
+          name: 'test child tag',
+          parentTagName: 'test parent tag',
+          colorCode: '#ffffff');
+
+      // Create parent tag
+      await tagRepository.createTag(parentTag);
+
+      // Check if parent tag is created
+      final actualParentCreated =
+          await tagRepository.getTagByName('test parent tag');
+      expect(actualParentCreated, equals(parentTag));
+
+      // Create child tag
+      await tagRepository.createTag(childTag);
+
+      // Check if child tag is created
+      final actualChildCreated =
+          await tagRepository.getTagByName('test child tag');
+      expect(actualChildCreated, equals(childTag));
+
+      // Get parent tag of child tag
+      final actualParents = await tagRepository.getParentTags(childTag);
+      expect(actualParents, equals([parentTag]));
+
+      // Get parent tag of parent tag
+      final actualParentParents = await tagRepository.getParentTags(parentTag);
+      expect(actualParentParents, isEmpty);
+    });
+  });
+}
