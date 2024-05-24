fe4e043caa02ca92a1f31c78254192bc4e929f23
Zepten <Zepten@yandex.ru>
2024-05-24 21:27:46 +0300

    remove unnesessary ColorCodeEntity, ColorCodeModel and ColorCodeDao

--- lib/features/file_explorer/data/data_sources/database/app_database.dart
+++ lib/features/file_explorer/data/data_sources/database/app_database.dart
@@ -2,8 +2,6 @@ import 'package:floor/floor.dart';
 import 'package:tagsurf_flutter/features/file_explorer/data/data_sources/database/DAO/file_dao.dart';
 import 'package:tagsurf_flutter/features/file_explorer/data/data_sources/database/DAO/file_tags_dao.dart';
 import 'package:tagsurf_flutter/features/file_explorer/data/data_sources/database/DAO/tag_dao.dart';
-import 'package:tagsurf_flutter/features/file_explorer/data/data_sources/database/DAO/color_code_dao.dart';
-import 'package:tagsurf_flutter/features/file_explorer/data/models/color_code.dart';
 import 'package:tagsurf_flutter/features/file_explorer/data/models/file.dart';
 import 'package:tagsurf_flutter/features/file_explorer/data/models/files_tags.dart';
 import 'package:tagsurf_flutter/features/file_explorer/data/models/tag.dart';
@@ -12,10 +10,9 @@ import 'dart:async';
 
 part 'app_database.g.dart';
 
-@Database(version: 1, entities: [FileModel, TagModel, ColorCodeModel, FilesTagsModel])
+@Database(version: 1, entities: [FileModel, TagModel, FilesTagsModel])
 abstract class AppDatabase extends FloorDatabase {
   FileDao get fileDao;
   TagDao get tagDao;
   FilesTagsDao get fileTagsDao;
-  ColorCodeDao get colorCodeDao;
 }
