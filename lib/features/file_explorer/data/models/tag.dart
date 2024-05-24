4072148a3d6e1cc6337a3271d0d84096e3db9432
Zepten <Zepten@yandex.ru>
2024-05-24 21:37:01 +0300

    remove foreign key for color code, rename parentTagName

--- lib/features/file_explorer/data/models/tag.dart
+++ lib/features/file_explorer/data/models/tag.dart
@@ -1,27 +1,22 @@
 import 'package:floor/floor.dart';
-import 'package:tagsurf_flutter/features/file_explorer/data/models/color_code.dart';
 import 'package:tagsurf_flutter/features/file_explorer/domain/entities/tag_entity.dart';
 
 @Entity(tableName: 'tags', primaryKeys: [
   'name'
 ], foreignKeys: [
   ForeignKey(
-      childColumns: ['parent_tag'], parentColumns: ['name'], entity: TagModel),
-  ForeignKey(
-      childColumns: ['color_code'],
-      parentColumns: ['color'],
-      entity: ColorCodeModel)
+      childColumns: ['parent_tag_name'], parentColumns: ['name'], entity: TagModel)
 ])
 class TagModel extends TagEntity {
   const TagModel(
       {required super.name,
-      required super.parentTag,
+      required super.parentTagName,
       required super.colorCode});
 
   factory TagModel.fromEntity(TagEntity tagEntity) {
     return TagModel(
       name: tagEntity.name,
-      parentTag: tagEntity.parentTag!,
+      parentTagName: tagEntity.parentTagName,
       colorCode: tagEntity.colorCode,
     );
   }
--- lib/features/file_explorer/domain/entities/tag_entity.dart
+++ lib/features/file_explorer/domain/entities/tag_entity.dart
@@ -4,22 +4,22 @@ import 'package:tagsurf_flutter/features/file_explorer/data/models/tag.dart';
 
 class TagEntity extends Equatable {
   final String? name;
-  @ColumnInfo(name: 'parent_tag') // TODO: put into FileModel or remove inheritance from Models
-  final String? parentTag;
+  @ColumnInfo(name: 'parent_tag_name') // TODO: put into FileModel or remove inheritance from Models
+  final String? parentTagName;
   @ColumnInfo(name: 'color_code')
   final String colorCode;
 
   const TagEntity(
-      {required this.name, required this.parentTag, required this.colorCode});
+      {required this.name, required this.parentTagName, required this.colorCode});
 
   factory TagEntity.fromModel(TagModel tagModel) {
     return TagEntity(
       name: tagModel.name,
-      parentTag: tagModel.parentTag,
+      parentTagName: tagModel.parentTagName,
       colorCode: tagModel.colorCode,
     );
   }
 
   @override
-  List<Object?> get props => [name, parentTag, colorCode];
+  List<Object?> get props => [name, parentTagName, colorCode];
 }
