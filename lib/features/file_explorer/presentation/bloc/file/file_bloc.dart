9ad95a993dcf9bccaf9c79120ce57153c23cbead
Zepten <Zepten@yandex.ru>
2024-05-25 19:10:46 +0300

    implement FileBloc

--- lib/features/file_explorer/presentation/bloc/file/file_bloc.dart
+++ lib/features/file_explorer/presentation/bloc/file/file_bloc.dart
@@ -2,19 +2,52 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:tagsurf_flutter/features/file_explorer/domain/entities/file_entity.dart';
 import 'package:tagsurf_flutter/features/file_explorer/domain/usecases/get_tracked_files.dart';
+import 'package:tagsurf_flutter/features/file_explorer/domain/usecases/track_file.dart';
+import 'package:tagsurf_flutter/features/file_explorer/domain/usecases/track_multiple_files.dart';
+import 'package:tagsurf_flutter/features/file_explorer/domain/usecases/untrack_file.dart';
 
 part 'file_event.dart';
 part 'file_state.dart';
 
 class FileBloc extends Bloc<FileEvent, FileState> {
-
+  // UseCases
   final GetTrackedFilesUseCase _getTrackedFilesUseCase;
+  final TrackFileUseCase _trackFileUseCase;
+  final UntrackFileUseCase _untrackFileUseCase;
+  final TrackFilesUseCase _trackMultipleFilesUseCase;
 
-  FileBloc(this._getTrackedFilesUseCase) : super(const FileLoadingState()) {
-    on<GetFileEvent>(onGetFiles);
+  FileBloc(this._getTrackedFilesUseCase, this._trackFileUseCase,
+      this._untrackFileUseCase, this._trackMultipleFilesUseCase)
+      : super(const FileLoadingState()) {
+    on<GetTrackedFilesEvent>(onGetTrackedFiles);
+    on<TrackFileEvent>(onTrackFile);
+    on<UntrackFileEvent>(onUntrackFile);
+    on<TrackFilesEvent>(onTrackFiles);
   }
 
-  void onGetFiles(GetFileEvent event, Emitter<FileState> emit) async {
+  void onGetTrackedFiles(
+      GetTrackedFilesEvent event, Emitter<FileState> emit) async {
+    final files = await _getTrackedFilesUseCase();
+    emit(FileLoadedState(files));
+  }
+
+  void onTrackFile(
+      TrackFileEvent trackFileEvent, Emitter<FileState> emit) async {
+    await _trackFileUseCase(params: trackFileEvent.file);
+    final files = await _getTrackedFilesUseCase();
+    emit(FileLoadedState(files));
+  }
+
+  void onTrackFiles(
+      TrackFilesEvent trackFilesEvent, Emitter<FileState> emit) async {
+    await _trackMultipleFilesUseCase(params: trackFilesEvent.files);
+    final files = await _getTrackedFilesUseCase();
+    emit(FileLoadedState(files));
+  }
+
+  void onUntrackFile(
+      UntrackFileEvent untrackFileEvent, Emitter<FileState> emit) async {
+    await _untrackFileUseCase(params: untrackFileEvent.file);
     final files = await _getTrackedFilesUseCase();
     emit(FileLoadedState(files));
   }
--- lib/features/file_explorer/presentation/bloc/file/file_event.dart
+++ lib/features/file_explorer/presentation/bloc/file/file_event.dart
@@ -1,9 +1,27 @@
 part of 'file_bloc.dart';
 
-abstract class FileEvent {
-  const FileEvent();
+abstract class FileEvent extends Equatable {
+  final FileEntity? file;
+  final List<FileEntity>? files;
+
+  const FileEvent({this.file, this.files});
+
+  @override
+  List<Object?> get props => [file!];
 }
 
-class GetFileEvent extends FileEvent {
-  const GetFileEvent();
+class GetTrackedFilesEvent extends FileEvent {
+  const GetTrackedFilesEvent();
+}
+
+class TrackFilesEvent extends FileEvent {
+  const TrackFilesEvent(List<FileEntity> files) : super(files: files);
+}
+
+class TrackFileEvent extends FileEvent {
+  const TrackFileEvent(FileEntity file) : super(file: file);
+}
+
+class UntrackFileEvent extends FileEvent {
+  const UntrackFileEvent(FileEntity file) : super(file: file);
 }
