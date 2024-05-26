f6640314c118b57f9af2ddb6831bb71986f25261
Zepten <Zepten@yandex.ru>
2024-05-26 21:15:05 +0300

    add color code, update null safety of TagEntity

--- lib/features/file_explorer/presentation/widgets/tag_widget.dart
+++ lib/features/file_explorer/presentation/widgets/tag_widget.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:tagsurf_flutter/features/file_explorer/domain/entities/tag_entity.dart';
 
 class TagWidget extends StatelessWidget {
-  final TagEntity? tag;
+  final TagEntity tag;
 
-  const TagWidget({super.key, this.tag});
+  const TagWidget({super.key, required this.tag});
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +12,12 @@ class TagWidget extends StatelessWidget {
       padding: const EdgeInsets.all(8),
       margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
       decoration: BoxDecoration(
-        color: Colors.white, // TODO: tag color code
+        color: Color.fromARGB(
+          255,
+          tag.colorCode.red,
+          tag.colorCode.green,
+          tag.colorCode.blue,
+        ),
         borderRadius: BorderRadius.circular(20),
         border: Border.all(color: Colors.black, width: 1),
       ),
@@ -20,18 +25,18 @@ class TagWidget extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Text(
-            tag!.name,
+            tag.name,
             style: const TextStyle(
               // TODO: contrast tag name color
               color: Colors.black,
               fontWeight: FontWeight.bold,
             ),
           ),
-          if (tag != null && tag!.parentTagName != null)
+          if (tag.parentTagName != null)
             Padding(
               padding: const EdgeInsets.only(top: 4),
               child: Text(
-                'Parent: ${tag!.parentTagName!}',
+                'Parent: ${tag.parentTagName}',
                 // Цвет текста, выбрать контрастирующий с цветом тега
                 style: const TextStyle(
                   color: Colors.black,
