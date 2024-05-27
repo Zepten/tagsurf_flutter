175cfc62f322cf6e204f9ddc309518c830dd58b6
Zepten <Zepten@yandex.ru>
2024-05-27 11:23:49 +0300

    add helper functions for Color and ColorCode

---
+++ lib/config/util/color_code.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:tagsurf_flutter/features/file_explorer/domain/entities/color_code.dart';
+
+Color getColorFromColorCode(ColorCode colorCode) {
+  return Color.fromARGB(255, colorCode.red, colorCode.green, colorCode.blue);
+}
+
+Color getLightShadeFromColorCode(ColorCode colorCode) {
+  final color = getColorFromColorCode(colorCode);
+  return Color.lerp(color, Colors.white, 0.85)!;
+}
+
+Color getContrastColorFromColorCode(ColorCode colorCode) {
+  final color = getColorFromColorCode(colorCode);
+  if (color == Colors.white || color == Colors.black) {
+    return Colors.black;
+  } else {
+    return Color.lerp(color, Colors.black, 0.4)!;
+  }
+}
