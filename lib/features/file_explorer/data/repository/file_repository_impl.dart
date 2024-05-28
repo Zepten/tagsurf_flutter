6cdc5264f82875248b6923166cde222b43f950cd
Zepten <Zepten@yandex.ru>
2024-05-28 17:00:28 +0300

    add error handling for in-use methods

--- lib/features/file_explorer/data/repository/file_repository_impl.dart
+++ lib/features/file_explorer/data/repository/file_repository_impl.dart
@@ -1,3 +1,6 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:tagsurf_flutter/features/file_explorer/core/error/exceptions.dart';
+import 'package:tagsurf_flutter/features/file_explorer/core/error/failure.dart';
 import 'package:tagsurf_flutter/features/file_explorer/data/data_sources/database/app_database.dart';
 import 'package:tagsurf_flutter/features/file_explorer/data/data_sources/file_system/file_system_service.dart';
 import 'package:tagsurf_flutter/features/file_explorer/data/models/file.dart';
@@ -12,56 +15,113 @@ class FileRepositoryImpl implements FileRepository {
 
   // File system methods implementation for files
   @override
-  Future<List<FileEntity>> getAllFilesFromDirectory(
+  Future<Either<Failure, List<FileEntity>>> getAllFilesFromDirectory(
       {required String targetDir}) async {
-    final filesModels =
-        await _fileSystemService.getFilesFromDirectory(targetDir);
-    return filesModels
-        .map((fileModel) => FileEntity.fromModel(fileModel))
-        .toList();
+    try {
+      final filesModels =
+          await _fileSystemService.getFilesFromDirectory(targetDir);
+      return Right(filesModels
+          .map((fileModel) => FileEntity.fromModel(fileModel))
+          .toList());
+    } on FileSystemException {
+      return Left(FileSystemFailure());
+    }
   }
 
   // Database methods implementation for files
   @override
-  Future<void> trackFile({required FileEntity file}) async {
-    _appDatabase.fileDao.insertFile(FileModel.fromEntity(file));
+  Future<Either<Failure, void>> trackFile({required FileEntity file}) async {
+    try {
+      if (await _fileSystemService.isFileExist(FileModel.fromEntity(file))) {
+        await _appDatabase.fileDao.insertFile(FileModel.fromEntity(file));
+        return const Right(null);
+      } else {
+        return Left(FilesNotFoundFailure(paths: [file.path]));
+      }
+    } on DatabaseException {
+      return Left(DatabaseFailure());
+    }
   }
 
   @override
-  Future<void> trackFiles({required List<FileEntity> files}) async {
-    final filesModels =
-        files.map((fileEntity) => FileModel.fromEntity(fileEntity)).toList();
-    _appDatabase.fileDao.insertFiles(filesModels);
+  Future<Either<Failure, void>> trackFiles(
+      {required List<FileEntity> files}) async {
+    try {
+      final filesModels =
+          files.map((fileEntity) => FileModel.fromEntity(fileEntity)).toList();
+      final notExistFilesPaths =
+          await _fileSystemService.getNotExistFilesPaths(filesModels);
+      if (notExistFilesPaths.isEmpty) {
+        await _appDatabase.fileDao.insertFiles(filesModels);
+        return const Right(null);
+      } else {
+        return Left(FilesNotFoundFailure(paths: notExistFilesPaths));
+      }
+    } on DatabaseException {
+      return Left(DatabaseFailure());
+    }
   }
 
   @override
-  Future<void> untrackFile({required FileEntity file}) async {
-    _appDatabase.fileDao.deleteFile(FileModel.fromEntity(file));
+  Future<Either<Failure, void>> untrackFile({required FileEntity file}) async {
+    try {
+      await _appDatabase.fileDao.deleteFile(FileModel.fromEntity(file));
+      return const Right(null);
+    } on DatabaseException {
+      return Left(DatabaseFailure());
+    }
   }
 
   @override
-  Future<List<FileEntity>> getTrackedFiles() async {
-    final filesModels = await _appDatabase.fileDao.getAllFiles();
-    return filesModels
-        .map((fileModel) => FileEntity.fromModel(fileModel))
-        .toList();
+  Future<Either<Failure, List<FileEntity>>> getTrackedFiles() async {
+    try {
+      final filesModels = await _appDatabase.fileDao.getAllFiles();
+      final notExistFilesPaths =
+          await _fileSystemService.getNotExistFilesPaths(filesModels);
+      if (notExistFilesPaths.isEmpty) {
+        return Right(filesModels
+            .map((fileModel) => FileEntity.fromModel(fileModel))
+            .toList());
+      } else {
+        return Left(FilesNotFoundFailure(paths: notExistFilesPaths));
+      }
+    } on DatabaseException {
+      return Left(DatabaseFailure());
+    }
   }
 
   @override
-  Future<FileEntity?> getTrackedFileByPath({required String path}) async {
-    final fileModel = await _appDatabase.fileDao.getFileByPath(path);
-    return fileModel == null ? null : FileEntity.fromModel(fileModel);
+  Future<Either<Failure, FileEntity>> getTrackedFileByPath(
+      {required String path}) async {
+    // TODO: better error handling
+    try {
+      final fileModel = await _appDatabase.fileDao.getFileByPath(path);
+      if (fileModel != null) {
+        return Right(FileEntity.fromModel(fileModel));
+      } else {
+        return Left(DatabaseFailure());
+      }
+    } on DatabaseException {
+      return Left(DatabaseFailure());
+    }
   }
 
   @override
-  Future<List<FileEntity>> getUntrackedFilesFromDirectory(
+  Future<Either<Failure, List<FileEntity>>> getUntrackedFilesFromDirectory(
       {required String targetDir}) async {
-    final filesModelsFromFs =
-        await _fileSystemService.getFilesFromDirectory(targetDir);
-    final filesModelsFromDb = await _appDatabase.fileDao.getAllFiles();
-    return filesModelsFromFs
-        .where((fileModel) => !filesModelsFromDb.contains(fileModel))
-        .map((fileModel) => FileEntity.fromModel(fileModel))
-        .toList();
+    // TODO: better error handling
+    try {
+      final filesModelsFromFs =
+          await _fileSystemService.getFilesFromDirectory(targetDir);
+      final filesModelsFromDb = await _appDatabase.fileDao.getAllFiles();
+      return Right(filesModelsFromFs
+          .where((fileModel) => !filesModelsFromDb.contains(fileModel))
+          .map((fileModel) => FileEntity.fromModel(fileModel))
+          .toList());
+    } on FileSystemException {
+      return Left(FileSystemFailure());
+    } on DatabaseException {
+      return Left(DatabaseFailure());
+    }
   }
 }
