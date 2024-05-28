b400f00c155e21b49b087cb9249b4470ee05c591
Zepten <Zepten@yandex.ru>
2024-05-28 16:57:21 +0300

    add isFileExist and getNotExistFilesPaths methods, remove unnecessary throw Exception

--- lib/features/file_explorer/data/data_sources/file_system/file_system_service.dart
+++ lib/features/file_explorer/data/data_sources/file_system/file_system_service.dart
@@ -1,24 +1,36 @@
 import 'dart:io';
 
+import 'package:fpdart/fpdart.dart';
 import 'package:tagsurf_flutter/features/file_explorer/data/models/file.dart';
 
 abstract class FileSystemService {
   Future<List<FileModel>> getFilesFromDirectory(String targetDir);
+  Future<bool> isFileExist(FileModel file);
+  Future<List<String>> getNotExistFilesPaths(List<FileModel> files);
 }
 
 class FileSystemServiceImpl implements FileSystemService {
   @override
   Future<List<FileModel>> getFilesFromDirectory(String targetDir) async {
-    final directory = Directory(targetDir);
-    if (!await directory.exists()) {
-      // TODO: better error handling
-      throw Exception('Directory does not exists.');
-    }
-    final files = directory
+    final files = Directory(targetDir)
         .listSync(recursive: true)
         .whereType<File>()
         .map((file) => FileModel(path: file.path))
         .toList();
     return files;
+  }
+
+  @override
+  Future<bool> isFileExist(FileModel file) async {
+    return File(file.path).exists();
+  }
+
+  @override
+  Future<List<String>> getNotExistFilesPaths(List<FileModel> files) async {
+    final notExistFilePaths = files
+        .filter((file) => File(file.path).existsSync())
+        .map((file) => file.path)
+        .toList();
+    return notExistFilePaths;
   }
 }
