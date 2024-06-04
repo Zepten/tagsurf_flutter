42a1f09f8ffb4fce3fadcf8c32aea070a16d0741
Zepten <Zepten@yandex.ru>
2024-06-04 08:34:42 +0300

    add constants

---
+++ lib/config/constants/color_codes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const List<Color> availableTagColors = [
+  Color.fromARGB(255, 255, 255, 255),
+  Color.fromARGB(255, 255, 0, 0),
+  Color.fromARGB(255, 255, 128, 0),
+  Color.fromARGB(255, 255, 255, 0),
+  Color.fromARGB(255, 0, 255, 0),
+  Color.fromARGB(255, 0, 255, 255),
+  Color.fromARGB(255, 0, 0, 255),
+  Color.fromARGB(255, 255, 0, 255),
+];
+
+final defaultTagColor = availableTagColors[0];
