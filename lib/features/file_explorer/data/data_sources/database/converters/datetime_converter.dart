ff7374c30baef88bd5e1fc626a54f19a779bba1d
Zepten <Zepten@yandex.ru>
2024-06-06 16:42:56 +0300

    Revert "implementation not extension"
    This reverts commit a22a4cbfe856f3ba659ab14c6f6435f97a2ad398.

--- lib/features/file_explorer/data/data_sources/database/converters/datetime_converter.dart
+++ lib/features/file_explorer/data/data_sources/database/converters/datetime_converter.dart
@@ -1,6 +1,6 @@
 import 'package:floor/floor.dart';
 
-class DateTimeConverter implements TypeConverter<DateTime, int> {
+class DateTimeConverter extends TypeConverter<DateTime, int> {
   @override
   DateTime decode(int databaseValue) {
     return DateTime.fromMillisecondsSinceEpoch(databaseValue);
