6b3b4a89d4d17f0fd0c5be3995b6ffc85b0b9eb9
Zepten <Zepten@yandex.ru>
2024-06-28 02:32:37 +0300

    add placeholder actions for settings dialog, make dialog not dismissible

--- lib/features/file_explorer/presentation/widgets/common/dialogs/settings_dialog.dart
+++ lib/features/file_explorer/presentation/widgets/common/dialogs/settings_dialog.dart
@@ -3,9 +3,32 @@ import 'package:flutter/material.dart';
 Future<void> showSettingsDialog(BuildContext context) async {
   return showDialog(
     context: context,
+    barrierDismissible: false,
     builder: (BuildContext dialogContext) => AlertDialog(
       title: const Text('Настройки'),
       content: const Text('Меню настроек'),
+      actions: <Widget>[
+        TextButton(
+          child: const Text('ОК'),
+          onPressed: () {
+            // TODO: применить настройки и выйти из настроек
+            Navigator.of(dialogContext).pop();
+          },
+        ),
+        TextButton(
+          child: const Text('Отмена'),
+          onPressed: () {
+            // TODO: отменить настройки и выйти из настроек
+            Navigator.of(dialogContext).pop();
+          },
+        ),
+        TextButton(
+          child: const Text('Применить'),
+          onPressed: () {
+            // TODO: применить настройки
+          },
+        ),
+      ],
     ),
   );
 }
